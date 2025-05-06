09217706e9a392b41fdbd818fff8af329d524038
Dan Schuring <danschuringmeu@gmail.com>
2025-05-06 09:23:13 -0600

    Nate's Portfolio v2
    This is the next version of Nate's portfolio. It will include work from college and his BFA, and the formatting is updated.

---
+++ NatesPortfolio - V2/nates-old-as-of-4-30-2025.js
@@ -0,0 +1,88 @@
+/* allows left and right arrow keys and esc key to work slideshow */
+window.addEventListener("keydown", function(event) {
+  if (event.defaultPrevented) {
+    return; // Do nothing if event already handled
+  }
+
+  if (event.code === "ArrowLeft") {
+    plusSlides(-1);
+  } else if (event.code === "ArrowRight") {
+    plusSlides(1);
+  } else if (event.code === "Escape") {
+    closeModal();
+  }
+
+  refresh();
+
+  // Consume the event so it doesn't get handled twice
+  event.preventDefault();
+}, true);
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+// showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+document.getElementById("container").addEventListener("click", toggleMenu);
+function toggleMenu() {
+  let x = document.getElementById("menu");
+  console.log(x.className);
+  if (x.className === "menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "menu";
+  }
+}
+
+document.getElementById("gall-toggle").addEventListener("click", galleryToggle);
+function galleryToggle() {
+  let x = document.getElementById("gall-menu");
+  console.log(x.className);
+  if (x.className === "gall-menu") {
+    x.className += " responsive";
+  } else {
+    x.className = "gall-menu";
+  }
+}
+
+
+
+
+
+
---
+++ NatesPortfolio - V2/readme.md
@@ -0,0 +1,9 @@
+# Nate’s Ceramics Portfolio Repository.
+
+This repository (https://github.com/danschuring/Nates-Ceramics) is where I maintain the files for my son’s ceramics portfolio. The portfolio cannot be accessed for view directly from the repository but can be seen at
+https://nates-ceramics.onrender.com/
+
+[Nate’s Portfolio](https://nates-ceramics.onrender.com/) deploys and updates automatically on Render.com from this Github repository when I commit changes to the repository.
+
+
+
